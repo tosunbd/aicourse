b60da90952d9f0b18ff8ea85ce7c6a81448c863e
Taufiqul Islam <tosunbd@gmail.com>
2025-09-22 15:13:26 +0600

    Created the Frontend and Manage OPEN Ai API Key

---
+++ packages/server/index.ts
@@ -0,0 +1,19 @@
+import express from "express";  
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('Hello World!');
+    // res.send(process.env.OPENAI_API_KEY);
+}); 
+
+
+app.listen(port, () => {
+    console.log(`Server app listening on http://localhost:${port}`);
+});
