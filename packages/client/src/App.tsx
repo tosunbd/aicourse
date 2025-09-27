7eccb51eca78eff21e338ed94b861070f09bf604
tosunbd <tosunbd@gmail.com>
2025-09-27 22:38:53 +0600

    setup of tailwind css completed

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching message:', error));
   }, []);
-  return <p>{message}</p>
+  return <p className='font-bold p-4 text-3xl'>{message}</p>
 }
 
 export default App
