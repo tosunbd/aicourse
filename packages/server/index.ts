b37198edc7e55653cf8f0ef1716ebbe9721d8ed3
Taufiqul Islam <tosunbd@gmail.com>
2025-09-28 17:44:11 +0600

    Missing root index.ts file added.

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -7,17 +7,15 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-
 app.get("/", (req: Request, res: Response) => {
-    res.send('Hello World!');
-    // res.send(process.env.OPENAI_API_KEY);
+  res.send("Hello World!");
+  // res.send(process.env.OPENAI_API_KEY);
 });
 
 app.get("/api/hello", (req: Request, res: Response) => {
-    res.json({ message: 'Hello World!' });
+  res.json({ message: "Hello World!" });
 });
 
-
 app.listen(port, () => {
-    console.log(`Server app listening on http://localhost:${port}`);
+  console.log(`Server app listening on http://localhost:${port}`);
 });
