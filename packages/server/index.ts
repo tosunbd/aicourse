fefefac5ba639061aaf5038ae021e42cb96d3c3d
Taufiqul Islam <tosunbd@gmail.com>
2025-09-29 17:22:08 +0600

    added /api/chat

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import OpenAI from 'openai';
 
 dotenv.config();
 
+const client = new OpenAI({
+   apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
@@ -14,6 +20,18 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/api/hello', (req: Request, res: Response) => {
    res.json({ message: 'Hello World!' });
+});
+
+app.post('/api/chat', async (req: Request, res: Response) => {
+   const { prompt } = req.body;
+   const response = await client.responses.create({
+      model: 'gpt-4o-mini',
+      input: prompt,
+      temperature: 0.2,
+      max_output_tokens: 100,
+   });
+   // res.json({ message: response.output[0].content[0].text });
+   res.json({ message: response.output_text });
 });
 
 app.listen(port, () => {
